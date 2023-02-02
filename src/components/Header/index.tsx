c7d1e6bbcb25c27981b0506eb144a25222696840
mrchanghao <lnl2048@gmail.com>
2023-02-02 20:17:37 +0900

    package manager 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useMemo } from "react";
+import { ChangeEvent, FormEvent, useCallback, useMemo } from "react";
 import { useRecoilState } from "recoil";
 import { SearchState } from "../../atom/searchState";
 import { Input } from "../Input";
@@ -8,26 +8,27 @@ import { useState } from "react";
 
 // 스타일 수정
 export const Header = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [search, setSearch] = useRecoilState(SearchState);
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     // setSearch(e.target.value);
     setQuery(e.target.value);
-  };
-
-  
+  }, []);
 
   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setSearch(query);
-    
   };
 
   return (
     <TopHeader>
       <div>
         <Form onSubmit={(e) => submitHandler(e)}>
-          <Input placeholder="repo 이름을 검색해주세요" value={query} onChange={onChangeHandler} />
+          <Input
+            placeholder="repo 이름을 검색해주세요"
+            value={query}
+            onChange={onChangeHandler}
+          />
           <Button type="submit">search</Button>
         </Form>
       </div>
